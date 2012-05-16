c1145017910c0ae05308c2b1430b8dc46a9aa669
Dave Simons <dave@inuits.eu>
2012-05-16 14:40:49 +0200

    include params.pp
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,10 +24,12 @@
 #	}
 #
 class mumble {
+  include mumble::params
   include mumble::iptables
   include mumble::install
   include mumble::service
 
+  Class['mumble::params'] ->
   Class['mumble::iptables'] ->
   Class['mumble::install'] ->
   Class['mumble::service']
