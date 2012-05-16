e8968efd2541db97fe46eb7f3f350e889ddabaaa
Dave Simons <dave@inuits.eu>
2012-05-16 13:53:40 +0200

    add password and port params
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,20 @@
 class mumble::params (
   $install_type = undef,
+  $password     = undef,
+  $port         = undef,
 ) {
   $_install_type = $mumble::params::install_type ? {
     undef   => 'rpm',
     default => ${mumble::params::install_type},
+  }
+
+  $_password = $mumble::params::password ? {
+    undef   => 'letsmumble',
+    default => $mumble::params::password,
+  }
+
+  $_port = $mumble::params::port ? {
+    undef   => '64738',
+    default => $mumble::params::port,
   }
 }
